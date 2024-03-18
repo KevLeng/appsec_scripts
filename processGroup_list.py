69c2dd1a75be5e081b491566db03fc77c85556d6
kevleng <kevin.leng@dynatrace.com>
2024-03-18 15:21:08 +1100

    appsec monaco creator initial commit

---
+++ processGroup_list.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+import sys
+from argparse import ArgumentParser
+import csv
+from dynatrace_api import DynatraceApi
+import logging
+import logging.config
+from datetime import datetime
+logging.basicConfig(filename='output.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# get the Dynatrace Environmemnt (URL) and the API Token with arguments
+parser = ArgumentParser()
+parser.add_argument("-e", "--env", dest="environment", help="The Dynatrace Environment to query", required=True)
+parser.add_argument("-t", "--token", dest="token", help="The Dynatrace API Token to use", required=True)
+parser.add_argument("--debug", dest="debug", help="Set log level to debbug", action='store_true')
+parser.add_argument("-k", "--insecure", dest="insecure", help="Skip SSL certificate validation", action='store_true')
+parser.add_argument("-f", "--file", dest="file", help="Filename for output, default is processGroup_list.csv", default="processGroup_list.csv")
+
+parser.add_argument("--tag", dest="tag", help="Process Group Tag to filter by")
+parser.add_argument("--mz", dest="mz", help="Management Zone to filter by")
+parser.add_argument("--host", dest="host", help="Host to filter by")
+parser.add_argument("--name", dest="name", help="Filters by the name of the PG, uses a startswith logic")
+
+
+args = parser.parse_args()
+
+env = args.environment
+apiToken = args.token
+verifySSL = not args.insecure
+debug = args.debug
+
+fileName = args.file
+
+tag = args.tag
+mz = args.mz
+host = args.host
+name = args.name
+
+if debug:
+    logging.getLogger().setLevel(logging.DEBUG)
+
+logging.info("="*200)
+logging.info("Running %s ", " ".join(sys.argv))
+logging.info("="*200)
+
+#processTypes = ['DOTNET', 'IIS_APP_POOL', 'JAVA', 'NODE_JS', 'PHP']
+#CLV and RAP only support JAVA at the moment
+processTypes = "JAVA"
+
+dynatraceApi = DynatraceApi(env, apiToken, verifySSL)
+
+def getSelectorURL():
+    # split the list into chunks of 100 in order to avoid too large requests (URI too long)
+    url = "/api/v2/entities?pageSize=500&entitySelector=type(PROCESS_GROUP)"
+    url += f",softwareTechnologies({processTypes})"
+
+    if mz:
+        url += ",mzName(" + mz + ")"
+    if tag:
+        url += ",tag(" + tag + ")"
+    if host:
+        url += ",fromRelationships.runsOn(entityId(" + host + "))"
+    if name:
+         url += ",entityName.startsWith(" + name + ")"
+    url += "&fields=+fromRelationships,+properties,+managementZones&from=now-365d"
+
+    return url
+
+print("start")
+
+url = getSelectorURL()
+#processGroups = dynatraceApi.getAllEntities('/api/v2/entities?pageSize=500&&fields=+managementZones&entitySelector=type("PROCESS_GROUP"),mzName("EasyTrade","AppSec: Unguard","Cloud: Google"),softwareTechnologies("JAVA")&from=now-2h')
+
+#processGroups = dynatraceApi.getAllEntities('/api/v2/entities?pageSize=500&&fields=+managementZones&entitySelector=type(PROCESS_GROUP),softwareTechnologies(JAVA)&from=now-365d')
+
+#processGroups = dynatraceApi.getAllEntities('/api/v2/entities?pageSize=500&&fields=+fromRelationships,+properties,+managementZones&entitySelector=type(PROCESS_GROUP),mzName(allservers),softwareTechnologies(JAVA)')
+#processGroups = dynatraceApi.getAllEntities('/api/v2/entities?pageSize=500&entitySelector=type(PROCESS_GROUP),softwareTechnologies(JAVA),mzName(allservers)&fields=+fromRelationships,+properties,+managementZones')
+processGroups = dynatraceApi.getAllEntities(url)
+
+
+print(url)
+
+with open(fileName, "w", newline='') as f:
+    writer = csv.writer(f)
+    
+    for pg in processGroups:
+        print(pg["entityId"])
+        writer.writerow([pg["entityId"]])
+
+print("end")
---
+++ processGroup_monaco.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python
+import sys
+import os
+from argparse import ArgumentParser
+import csv
+from dynatrace_api import DynatraceApi
+import logging
+import logging.config
+from datetime import datetime
+import json
+
+logging.basicConfig(filename='output.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# get the Dynatrace Environmemnt (URL) and the API Token with arguments
+parser = ArgumentParser()
+parser.add_argument("-e", "--env", dest="environment", help="The Dynatrace Environment to query", required=True)
+parser.add_argument("-t", "--token", dest="token", help="The Dynatrace API Token to use", required=True)
+parser.add_argument("--debug", dest="debug", help="Set log level to debug", action='store_true')
+parser.add_argument("-k", "--insecure", dest="insecure", help="Skip SSL certificate validation", action='store_true')
+parser.add_argument('-f', '--file', help="CSV file containing the Process Group in the first column (no header)")
+parser.add_argument('-m', '--monaco', help="Folder to place the monaco configuration in, defaults to monaco in the local folder", default="monaco")
+parser.add_argument('-p', '--project', help="Project name for the monaco configuration, defaults to appsec", default="appsec")
+
+
+args = parser.parse_args()
+
+env = args.environment
+apiToken = args.token
+verifySSL = not args.insecure
+debug = args.debug
+filename = args.file
+monacoFolder = args.monaco
+projectFolder = args.project
+
+
+if debug:
+    logging.getLogger().setLevel(logging.DEBUG)
+
+logging.info("="*200)
+logging.info("Running %s ", " ".join(item if not "--token" in item else item for item in sys.argv))
+logging.info("="*200)
+
+for t in sys.argv:
+    print(t)
+dynatraceApi = DynatraceApi(env, apiToken, verifySSL)
+
+def createManifest():
+    content = f"""manifestVersion: 1.0
+    
+projects:
+    - name: {projectFolder}
+      path: {projectFolder}
+
+environmentGroups:
+    - name: target
+      environments:
+        - name: target-environment
+          url:
+            type: environment
+            value: DT_TENANT_URL
+          auth:
+            token:
+              name: DT_API_TOKEN
+    """
+    filename = monacoFolder + "/manifest.yaml"
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+    with open(filename, 'w', newline='') as f:
+        f.write(content)
+
+def createCLV_json():
+    clv_json = {
+        "enabled": True,
+        "criteria": {
+          "processGroup": "{{ .PGId }}"
+        },
+        "vulnerabilityDetectionControl": {
+          "monitoringMode": "MONITORING_ON"
+        },
+        "metadata": {
+          "comment": "Created by monaco"
+        }
+    }
+    filename = monacoFolder + "/" + projectFolder + "/code-level-vulnerability-rule.json"
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+    with open(filename, 'w', newline='') as f:
+        f.write(json.dumps(clv_json, indent=4))
+
+def createRAP_json():
+    rap_json = {
+        "enabled": True,
+        "criteria": {
+          "processGroup":  "{{ .PGId }}",
+          "attackType": "ANY"
+        },
+        "attackHandling": {
+          "blockingStrategy": "MONITOR"
+        },
+        "metadata": {
+          "comment": "Created by monaco"
+        }
+    }
+
+    filename = monacoFolder + "/" + projectFolder + "/attack-protection-advanced-rule.json"
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+    with open(filename, 'w', newline='') as f:
+        f.write(json.dumps(rap_json, indent=4))
+
+def createConfig():
+
+    config = "configs:"
+
+    filename = monacoFolder + "/" + projectFolder + "/_config.yaml"
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+    with open(filename, 'w', newline='') as f:
+        f.write(config)
+
+def appendConfig(processGroup, schema, template, function):
+    #print(processGroup)
+    config = f"""
+    - id: id_{function}_{processGroup}
+      type:
+        settings:
+          schema: {schema}
+          scope: environment
+      config:
+        name: {function}_{processGroup}
+        template: {template}
+        parameters:
+          {function.upper()}Enabled: 
+            name: {function.upper()}ENABLED
+            type: environment
+            default: true
+          PGId: {processGroup}
+    """
+    filename = monacoFolder + "/" + projectFolder + "/_config.yaml"
+    with open(filename, "a", newline="") as f:
+        f.write(config)
+
+print("start")
+
+if filename:
+    pgIds = []
+    with open(filename, newline='') as csvfile:
+        filereader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in filereader:
+            pgIds.append(row[0])
+            
+createManifest()
+createCLV_json()
+createRAP_json()
+createConfig()
+
+for pg in pgIds:
+    print(pg)
+    appendConfig(pg, "builtin:appsec.code-level-vulnerability-rule-settings", "code-level-vulnerability-rule.json", "clv")
+    appendConfig(pg, "builtin:appsec.attack-protection-advanced-config", "attack-protection-advanced-rule.json", "rap")
+
+
+print("end")
