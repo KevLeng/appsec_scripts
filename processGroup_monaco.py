eb0f4cdad520fb5d7e738827393e9f0f7a0265e1
kevleng <kevin.leng@dynatrace.com>
2024-03-20 14:14:50 +1100

    added oneagent feature config

--- processGroup_monaco.py
+++ processGroup_monaco.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import sys
 import os
+import argparse
 from argparse import ArgumentParser
 import csv
 from dynatrace_api import DynatraceApi
@@ -13,24 +14,32 @@ logging.basicConfig(filename='output.log', level=logging.INFO, format='%(asctime
 
 # get the Dynatrace Environmemnt (URL) and the API Token with arguments
 parser = ArgumentParser()
-parser.add_argument("-e", "--env", dest="environment", help="The Dynatrace Environment to query", required=True)
-parser.add_argument("-t", "--token", dest="token", help="The Dynatrace API Token to use", required=True)
+#parser.add_argument("-e", "--env", dest="environment", help="The Dynatrace Environment to create the monaco manifest", required=True)
+#parser.add_argument("-t", "--token", dest="token", help="The Dynatrace API Token to use", required=True)
 parser.add_argument("--debug", dest="debug", help="Set log level to debug", action='store_true')
 parser.add_argument("-k", "--insecure", dest="insecure", help="Skip SSL certificate validation", action='store_true')
 parser.add_argument('-f', '--file', help="CSV file containing the Process Group in the first column (no header)")
 parser.add_argument('-m', '--monaco', help="Folder to place the monaco configuration in, defaults to monaco in the local folder", default="monaco")
 parser.add_argument('-p', '--project', help="Project name for the monaco configuration, defaults to appsec", default="appsec")
+parser.add_argument('--clv', help="Include Code-level vulnerabilities configuration in monaco projection, defaults to true", action=argparse.BooleanOptionalAction, default=True)
+parser.add_argument('--rap', help="Include Runtime Application Protection configuration in monaco projection, defaults to true", action=argparse.BooleanOptionalAction, default=True)
+parser.add_argument('--oaf', help="Include OneAgent Features configuration in monaco projection, defaults to false", action=argparse.BooleanOptionalAction, default=False)
+
 
 
 args = parser.parse_args()
 
-env = args.environment
-apiToken = args.token
+#env = args.environment
+#apiToken = args.token
 verifySSL = not args.insecure
 debug = args.debug
 filename = args.file
 monacoFolder = args.monaco
 projectFolder = args.project
+includeCLV = args.clv
+includeRAP = args.rap
+includeOAF = args.oaf
+
 
 
 if debug:
@@ -40,9 +49,8 @@ logging.info("="*200)
 logging.info("Running %s ", " ".join(sys.argv))
 logging.info("="*200)
 
-for t in sys.argv:
-    print(t)
-dynatraceApi = DynatraceApi(env, apiToken, verifySSL)
+
+#dynatraceApi = DynatraceApi(env, apiToken, verifySSL)
 
 def createManifest():
     content = f"""manifestVersion: 1.0
@@ -105,6 +113,26 @@ def createRAP_json():
     with open(filename, 'w', newline='') as f:
         f.write(json.dumps(rap_json, indent=4))
 
+def createOAF_json():
+    JAVA_CASP_FLAW_FINDER_IAST = {"enabled": True,
+        "key": "JAVA_CASP_FLAW_FINDER_IAST"
+    }
+
+    SENSOR_JAVA_CASP_FLAW_FINDER = {"enabled": True,
+        "instrumentation": True,
+        "key": "SENSOR_JAVA_CASP_FLAW_FINDER"
+    }
+
+    filename = monacoFolder + "/" + projectFolder + "/java-code-level-vulnerability-evaluation.json"
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+    with open(filename, 'w', newline='') as f:
+        f.write(json.dumps(JAVA_CASP_FLAW_FINDER_IAST, indent=4))
+    
+    filename = monacoFolder + "/" + projectFolder + "/java-code-level-attack-evaluation.json"
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+    with open(filename, 'w', newline='') as f:
+        f.write(json.dumps(SENSOR_JAVA_CASP_FLAW_FINDER, indent=4))
+
 def createConfig():
 
     config = "configs:"
@@ -136,6 +164,22 @@ def appendConfig(processGroup, schema, template, function):
     with open(filename, "a", newline="") as f:
         f.write(config)
 
+def appendOAFConfig(processGroup, schema, template, function):
+    #print(processGroup)
+    config = f"""
+    - id: id_{function}_{processGroup}
+      type:
+        settings:
+          schema: {schema}
+          scope: {processGroup}
+      config:
+        name: {function}_{processGroup}
+        template: {template}
+    """
+    filename = monacoFolder + "/" + projectFolder + "/_config.yaml"
+    with open(filename, "a", newline="") as f:
+        f.write(config)
+
 print("start")
 
 if filename:
@@ -146,14 +190,36 @@ if filename:
             pgIds.append(row[0])
             
 createManifest()
-createCLV_json()
-createRAP_json()
+print(includeCLV)
+if includeCLV:
+    print("here")
+    logging.info("Creating Code-level vulnerabilities JSON file.")
+    createCLV_json()
+
+if includeRAP:
+    logging.info("Creating Runtime Application Protection JSON file.")
+    createRAP_json()
+
+
+if includeOAF:
+    logging.info("Creating OneAgent Feature JSON files.")
+    createOAF_json()
+
 createConfig()
 
 for pg in pgIds:
     print(pg)
-    appendConfig(pg, "builtin:appsec.code-level-vulnerability-rule-settings", "code-level-vulnerability-rule.json", "clv")
-    appendConfig(pg, "builtin:appsec.attack-protection-advanced-config", "attack-protection-advanced-rule.json", "rap")
+    if includeCLV:
+        logging.info("Creating Code-level vulnerabilities configuration.")
+        appendConfig(pg, "builtin:appsec.code-level-vulnerability-rule-settings", "code-level-vulnerability-rule.json", "clv")
+    if includeRAP:
+        logging.info("Creating Runtime Application Protection configuration.")
+        appendConfig(pg, "builtin:appsec.attack-protection-advanced-config", "attack-protection-advanced-rule.json", "rap")
+    if includeOAF:
+        logging.info("Creating OneAgent Feature configuration.")
+        appendOAFConfig(pg, "builtin:oneagent.features", "java-code-level-vulnerability-evaluation.json", "oaf_vul")
+        appendOAFConfig(pg, "builtin:oneagent.features", "java-code-level-attack-evaluation.json", "oaf_attack")
+
 
 
 print("end")
